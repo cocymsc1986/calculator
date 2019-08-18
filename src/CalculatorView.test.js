0f771b377875b47c36296df0cba7b6ad880f61be
Michael Cockcroft <cocymsc@hotmail.com>
2019-08-18 20:19:04 +0100

    adding tests

--- src/CalculatorView.test.js
+++ src/CalculatorView.test.js
@@ -4,13 +4,51 @@ import { shallow } from "enzyme";
 import CalculatorView from "./CalculatorView";
 
 describe("CalculatorView", () => {
-  it("should render", () => {});
+  it("should render", () => {
+    const wrapper = shallow(<CalculatorView />);
 
-  it("should call call onRegularClick when clicking a standard button", () => {});
+    expect(wrapper).toMatchSnapshot();
+  });
 
-  it("should call call onClearClick when clicking clear button", () => {});
+  it("should call onRegularClick when clicking a standard button", () => {
+    const onRegularClickMock = jest.fn();
 
-  it("should call call onCEClick when clicking CE button", () => {});
+    const wrapper = shallow(
+      <CalculatorView onRegularClick={onRegularClickMock} />
+    );
 
-  it("should call call onSumClick when clicking equals button", () => {});
+    wrapper.find("[data-test-id='number-1']").simulate("click");
+
+    expect(onRegularClickMock).toHaveBeenCalled();
+  });
+
+  it("should call onClearClick when clicking clear button", () => {
+    const onClearClickMock = jest.fn();
+
+    const wrapper = shallow(<CalculatorView onClearClick={onClearClickMock} />);
+
+    wrapper.find("[data-test-id='clear']").simulate("click");
+
+    expect(onClearClickMock).toHaveBeenCalled();
+  });
+
+  it("should call onCEClick when clicking CE button", () => {
+    const onCEClickMock = jest.fn();
+
+    const wrapper = shallow(<CalculatorView onCEClick={onCEClickMock} />);
+
+    wrapper.find("[data-test-id='ce']").simulate("click");
+
+    expect(onCEClickMock).toHaveBeenCalled();
+  });
+
+  it("should call onSumClick when clicking equals button", () => {
+    const onSumClickMock = jest.fn();
+
+    const wrapper = shallow(<CalculatorView onSumClick={onSumClickMock} />);
+
+    wrapper.find("[data-test-id='equals']").simulate("click");
+
+    expect(onSumClickMock).toHaveBeenCalled();
+  });
 });
